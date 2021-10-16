afd2165cddcc4878d158071c9acb32793e87afba
DinoRankine <daniellerankine13@gmail.com>
2021-10-16 14:42:25 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Practice_Events_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picGhost = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picGhost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblName.Location = new System.Drawing.Point(28, 96);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(69, 21);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 83);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(347, 34);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDisplay.Location = new System.Drawing.Point(28, 223);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(84, 21);
+            this.lblDisplay.TabIndex = 4;
+            this.lblDisplay.Text = "Display:";
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.Color.Lime;
+            this.btnShow.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnShow.Location = new System.Drawing.Point(32, 345);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(154, 85);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "&Show Name";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.Color.Aqua;
+            this.btnDisplay.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDisplay.Location = new System.Drawing.Point(212, 345);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(154, 85);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "&Display Title";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Violet;
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnReset.Location = new System.Drawing.Point(393, 345);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(154, 85);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(118, 210);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(347, 34);
+            this.txtDisplay.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Crimson;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnExit.Location = new System.Drawing.Point(573, 345);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(154, 85);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // picGhost
+            // 
+            this.picGhost.Image = ((System.Drawing.Image)(resources.GetObject("picGhost.Image")));
+            this.picGhost.InitialImage = null;
+            this.picGhost.Location = new System.Drawing.Point(552, 109);
+            this.picGhost.Name = "picGhost";
+            this.picGhost.Size = new System.Drawing.Size(100, 116);
+            this.picGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picGhost.TabIndex = 9;
+            this.picGhost.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(761, 461);
+            this.Controls.Add(this.picGhost);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Practice Program 2";
+            ((System.ComponentModel.ISupportInitialize)(this.picGhost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picGhost;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_Events_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            picGhost.Visible = false;
+        }
+
+      
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string name1 = txtName.Text;
+            if (name1 == string.Empty)
+            {
+                
+                MessageBox.Show("Please make an input in the name box","Error");
+            }
+
+            else
+            { 
+                //get input from Name text box and place it in the Display text box.
+                txtDisplay.Text = txtName.Text;
+                picGhost.Visible = true;
+                }
+            
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = this.Text;
+            picGhost.Visible = true;
+            txtName.Clear();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            txtName.Clear();
+            picGhost.Visible = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
